244f7c1e3d4b3231f4ab006690ca7b4477dc175b
Ishant <ishantpundir202@gmail.com>
2024-11-19 11:06:22 +0530

    tracing logging

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    // Initialize tracing.
+    if let Ok(env_filter) = tracing_subscriber::EnvFilter::try_from_default_env() {
+        tracing_subscriber::fmt()
+            .compact()
+            .with_env_filter(env_filter)
+            .init();
+    } else {
+        tracing_subscriber::fmt().compact().init();
+    }
+
+    tracing::info!("Welcome to Aurora!");
 }
